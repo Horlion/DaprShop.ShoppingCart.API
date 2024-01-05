3f55c96fdc9bcf001c55a7058ee282c814005f15
Sibaprasad Padhi <sibaprasad@pulllogic.com>
2024-01-05 17:41:23 +0530

    Add project files.

---
+++ DaprShop.Contracts.cs
@@ -0,0 +1,8 @@
+﻿namespace DaprShop.Contracts
+{
+    public class ProductItemAddedToShoppingCartEvent
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string ProductId { get; set; } = string.Empty;
+    }
+}
---
+++ Services/IShoppingCartService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace DaprShop.ShoppingCart.API.Services
+{
+    public interface IShoppingCartService
+    {
+        Task<Domain.ShoppingCart> GetShoppingCartAsync(string userId);
+        Task AddItemToShoppingCartAsync(string userId, Domain.ShoppingCartItem item);
+    }
+}
